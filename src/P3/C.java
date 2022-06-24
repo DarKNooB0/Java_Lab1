35e984091be1170398b34bf7b41bf2f7fa1b1fbf
DarKNooB0 <vongolabookrock@gmail.com>
2022-06-24 02:19:19 -0700

    first commit

---
+++ src/P3/C.java
@@ -0,0 +1,13 @@
+package P3;
+
+public class C {
+    public static void main(String[] args) {
+        String str1 ="Good";
+        String str2 ="Good goose";
+
+        System.out.println("str1 " + str1);
+        System.out.println("str2 " + str2);
+        System.out.println("str1 == str2 : " + str1.equals(str2));
+
+    }
+}
---
+++ src/P5/E.java
@@ -0,0 +1,11 @@
+package P5;
+
+public class E {
+    public static void main(String[] args) {
+        int x = 99999999;
+        int y = ~x;
+        System.out.println("x= " + x);
+        System.out.println("y= " + y);
+
+    }
+}
